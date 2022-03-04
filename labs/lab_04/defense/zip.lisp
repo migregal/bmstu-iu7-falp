0230dd8d7861a4e33848ef1cd479ff67193d1b74
migregal <devmigregal@gmail.com>
2022-03-04 21:49:04 +0300

    lab_04 defence refactor

--- labs/lab_04/defense/zip.lisp
+++ labs/lab_04/defense/zip.lisp
@@ -1,21 +1,11 @@
 ; (zip '((1 2 3) (4 5 6) (7 8 9))) ===> ((1 4 7) (2 5 8) (3 6 9))
 
 (defun my-mapcar (fn arg)
-    (if (null arg)
-        nil
-        (cons
-            (funcall fn (car arg))
-            (my-mapcar fn (cdr arg))
-        )
-    )
-)
+    (and arg (cons (funcall fn (car arg)) (my-mapcar fn (cdr arg)))))
 
 (defun zip (lists)
-    (if (null lists)
-        Nil
+    (and
+        lists
         (if (null (car lists))
             ()
-            (cons (my-mapcar #'car lists) (zip (my-mapcar #'cdr lists)))
-        )
-    )
-)
+            (cons (my-mapcar #'car lists) (zip (my-mapcar #'cdr lists))))))
