9028412a8061a4587f077ddc8a2027b7eeccce72
migregal <devmigregal@gmail.com>
2022-03-05 12:55:42 +0300

    lab_05: task 06 fixed

--- labs/lab_05/src/06.lisp
+++ labs/lab_05/src/06.lisp
@@ -1,14 +1,20 @@
-(defun swap-to-left (lst)
-  (let ((el (car lst)))
-    (append (cdr lst) (list el))))
+(defun swap-to-left-append (lst)
+  (and
+    lst
+    (listp lst)
+    (let ((el (car lst)))
+    (append (cdr lst) (list el)))))
 
 (defun swap-to-left (lst)
   (and
     lst
+    (listp lst)
     (let ((tail (cdr lst)) (head (car lst)))
       (nreverse (cons head (reverse tail))))))
 
 (defun swap-to-right (lst)
-  (and lst
+  (and
+    lst
+    (listp lst)
     (let ((last-el (car (last lst))))
       (nreverse (cdr (reverse (cons last-el lst)))))))
