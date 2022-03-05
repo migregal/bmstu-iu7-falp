884ad90352f0b94c4bdd1d99437ed817191b4f04
migregal <devmigregal@gmail.com>
2022-03-05 13:18:01 +0300

    lab_05: task 08 fixed

--- labs/lab_05/src/08.lisp
+++ labs/lab_05/src/08.lisp
@@ -1,9 +1,15 @@
 (defun mult-all-numbers (mult lst)
-  (mapcar #'(lambda (el) (* el mult)) lst))
+  (cond
+    ((not (numberp mult)) lst)
+    ((not (listp lst)) lst)
+    (T (mapcar #'(lambda (el) (* el mult)) lst))))
 
 (defun compl-mult-all-numbers (mult lst)
-  (mapcar
-    #'(lambda (el)
-        (cond ((listp el) (compl-mult-all-numbers mult el))
-              (T (* el mult))))
-    lst))
+  (cond
+    ((not (numberp mult)) lst)
+    ((not (listp lst)) lst)
+    (T (mapcar
+      #'(lambda (el)
+          (cond ((listp el) (compl-mult-all-numbers mult el))
+                (T (* el mult))))
+      lst))))
