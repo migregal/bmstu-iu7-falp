175b45e6e31497305c6ff4665fcda4ebd63a19df
migregal <devmigregal@gmail.com>
2022-03-05 12:21:36 +0300

    lab_05: task 04 fixed

--- labs/lab_05/src/04.lisp
+++ labs/lab_05/src/04.lisp
@@ -1,11 +1,17 @@
 (defun nswap-first-last (lst)
-  (let ((el1 (car lst)) (last-el (last lst)))
-    (setf (car lst) (car last-el))
-    (setf (car last-el) el1)
-    lst))
+  (if
+    (null lst)
+    ()
+    (let ((el1 (car lst)) (last-el (last lst)))
+      (setf (car lst) (car last-el))
+      (setf (car last-el) el1)
+      lst)))
 
 (defun swap-first-last (lst)
   (let
     ((el1 (car lst)) (last-el (car (last lst))))
-    (nreverse
-      (cons el1 (cdr (reverse (cons last-el (cdr lst))))))))
+    (if
+      (null lst)
+      ()
+      (nreverse
+        (cons el1 (cdr (reverse (cons last-el (cdr lst)))))))))
