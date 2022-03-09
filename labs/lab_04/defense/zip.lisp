77de7f1f215e2e943660fe5ed9231955e93e4c38
migregal <devmigregal@gmail.com>
2022-03-09 12:51:12 +0300

    lab_04: defense updated

--- labs/lab_04/defense/zip.lisp
+++ labs/lab_04/defense/zip.lisp
@@ -1,7 +1,25 @@
-; (zip '((1 2 3) (4 5 6) (7 8 9))) ===> ((1 4 7) (2 5 8) (3 6 9))
+; (zip '((1 2) (4 5 6) (7 8 9))) ===> ((1 4 7) (2 5 8) (3 6 9))
+
+(defun min-len (lst)
+    (cond
+        ((null (car lst)) 0)
+        ((null (cdr lst)) (length (car lst)))
+        (t (min
+            (length (car lst))
+            (min-len (cdr lst))))))
+
+(defun my-mapcar-internal (fn arg n)
+    (and
+        arg
+        (cons
+            (funcall fn (subseq (car arg) 0 n))
+            (my-mapcar-internal fn (cdr arg) n))))
 
 (defun my-mapcar (fn arg)
-    (and arg (cons (funcall fn (car arg)) (my-mapcar fn (cdr arg)))))
+    (let ((len (min-len arg)))
+        (and
+            arg
+            (my-mapcar-internal fn arg len))))
 
 (defun zip (lists)
     (and
